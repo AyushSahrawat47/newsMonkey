e0822be17947b35da3aafa2dc872ff51bfd5dba5
Ayush Sahrawat <ayushsahrawat47@gmail.com>
2024-04-27 21:51:58 +0530

    Basic structure of newsapp is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import React, { Component } from 'react'
+import Navbar from './componenets/Navbar';
+import News from './componenets/News'
 
-function App() {
-  return (
-    <>
-
-    </>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <News/>
+      </div>
+    )
+  }
 }
 
-export default App;
